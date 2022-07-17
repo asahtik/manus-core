f8e747766b1cdf1ef609e79df7ac4ef39890eb68
asahtik <asahtik@gmail.com>
2022-07-17 18:58:32 +0200

    Changed origin transformation

--- python/manus/__init__.py
+++ python/manus/__init__.py
@@ -113,8 +113,46 @@ class Manipulator(object):
         for s in self._listeners:
             s.on_planner_state(self, state)
 
+    def _mat_mult_vec(m, v):
+        return np.array([
+            m[0] * v[0] + m[1] * v[1] + m[2] * v[2],
+            m[3] * v[0] + m[4] * v[1] + m[5] * v[2],
+            m[6] * v[0] + m[7] * v[1] + m[8] * v[2]
+        ])
+
+    def _mat_mult_mat(m1, m2):
+        return np.array([
+            m1[0] * m2[0] + m1[1] * m2[3] + m1[2] * m2[6],
+            m1[0] * m2[1] + m1[1] * m2[4] + m1[2] * m2[7],
+            m1[0] * m2[2] + m1[1] * m2[5] + m1[2] * m2[8],
+            m1[3] * m2[0] + m1[4] * m2[3] + m1[5] * m2[6],
+            m1[3] * m2[1] + m1[4] * m2[4] + m1[5] * m2[7],
+            m1[3] * m2[2] + m1[4] * m2[5] + m1[5] * m2[8],
+            m1[6] * m2[0] + m1[7] * m2[3] + m1[8] * m2[6],
+            m1[6] * m2[1] + m1[7] * m2[4] + m1[8] * m2[7],
+            m1[6] * m2[2] + m1[7] * m2[5] + m1[8] * m2[8]
+        ])
+
+    def _vec_add_vec(v1, v2):
+        return np.array([
+            v1[0] + v2[0],
+            v1[1] + v2[1],
+            v1[2] + v2[2]
+        ])
+
     def transform(self, joint):
-        origin = np.identity(4)
+        origin = np.array([
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ])
+        rotation = np.array([
+            1, 0, 0,
+            0, 1, 0,
+            0, 0, 1
+        ])
+        translation = np.array([0, 0, 0])
         if self.description is None or joint < 0 or joint >= len(self.state.joints):
             return origin
         for j in xrange(0, joint+1):
@@ -126,20 +164,39 @@ class Manipulator(object):
                 self._description_data.joints[j].rp, \
                 self._description_data.joints[j].ry
 
+            qrotation = np.array([
+                1, 0, 0,
+                0, 1, 0,
+                0, 0, 1
+            ])
+            qtranslation = np.array([0, 0, 0])
+            q_pos = self.state.joints[j].position
             if self._description_data.joints[j].type == messages.JointType.ROTATION:
                 if self._description_data.joints[j].axis == messages.JointAxis.X:
-                    rr = rr + self.state.joints[j].position
+                    qrotation = np.array([
+                        1, 0, 0,
+                        0, np.cos(q_pos), -np.sin(q_pos),
+                        0, np.sin(q_pos), np.cos(q_pos)
+                    ])
                 elif self._description_data.joints[j].axis == messages.JointAxis.Y:
-                    rp = rp + self.state.joints[j].position
+                    qrotation = np.array([
+                        np.cos(q_pos), 0, np.sin(q_pos),
+                        0, 1, 0,
+                        -np.sin(q_pos), 0, np.cos(q_pos)
+                    ])
                 elif self._description_data.joints[j].axis == messages.JointAxis.Z:
-                    ry = ry + self.state.joints[j].position
+                    qrotation = np.array([
+                        np.cos(q_pos), -np.sin(q_pos), 0,
+                        np.sin(q_pos), np.cos(q_pos), 0,
+                        0, 0, 1
+                    ])
             elif self._description_data.joints[j].type == messages.JointType.TRANSLATION:
                 if self._description_data.joints[j].axis == messages.JointAxis.X:
-                    tx = tx + self.state.joints[j].position
+                    qtranslation = np.array([q_pos, 0, 0])
                 elif self._description_data.joints[j].axis == messages.JointAxis.Y:
-                    ty = ty + self.state.joints[j].position
+                    qtranslation = np.array([0, q_pos, 0])
                 elif self._description_data.joints[j].axis == messages.JointAxis.Z:
-                    tz = tz + self.state.joints[j].position
+                    qtranslation = np.array([0, 0, q_pos])
 
             ca1 = np.cos(ry)
             sa1 = np.sin(ry)
@@ -148,19 +205,21 @@ class Manipulator(object):
             cc1 = np.cos(rr)
             sc1 = np.sin(rr)
 
-            rotation = np.array(( \
-                (ca1*cb1, ca1*sb1*sc1 - sa1*cc1, ca1*sb1*cc1 + sa1*sc1, 0), \
-                (sa1*cb1, sa1*sb1*sc1 + ca1*cc1, sa1*sb1*cc1 - ca1*sc1, 0), \
-                (-sb1, cb1*sc1, cb1*cc1, 0), \
-                (0, 0, 0, 1) \
-            ))
-            translation = np.array(( \
-                (0, 0, 0, tx), \
-                (0, 0, 0, ty), \
-                (0, 0, 0, tz), \
-                (0, 0, 0, 1) \
-            ))
+            lrotation = np.array([
+                ca1*cb1, ca1*sb1*sc1 - sa1*cc1, ca1*sb1*cc1 + sa1*sc1,
+                sa1*cb1, sa1*sb1*sc1 + ca1*cc1, sa1*sb1*cc1 - ca1*sc1,
+                -sb1, cb1*sc1, cb1*cc1
+            ])
+            ltranslation = np.array([tx, ty, tz])
 
-            origin = np.add(np.matmul(rotation, origin), translation)
+            ltranslation = self.vec_add_vec(qtranslation, self.mat_mult_vec(qrotation, ltranslation))
+            lrotation = self.mat_mult_mat(qrotation, lrotation)
+            translation = self.vec_add_vec(translation, self.mat_mult_vec(rotation, ltranslation))
+            rotation = self.mat_mult_mat(rotation, lrotation)
 
-        return origin
+        return np.array([
+            [rotation[0], rotation[1], rotation[2], translation[0]],
+            [rotation[3], rotation[4], rotation[5], translation[1]],
+            [rotation[6], rotation[7], rotation[8], translation[2]],
+            [0, 0, 0, 1]
+        ])
